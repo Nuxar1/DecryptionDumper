bb9e5c9c35c29c28b53dbf3ac9ac99adc897d416
Mats Büscher <mats.buescher@gmx.de>
2021-11-12 14:50:44 +0100

    visual studio update, endless loop

--- DecryptionDumper/DecryptionDumper.cpp
+++ DecryptionDumper/DecryptionDumper.cpp
@@ -9,8 +9,11 @@ int main()
 	debug.Init("C:\\Program Files (x86)\\Call of Duty Modern Warfare\\ModernWarfare.exe");
 	debug.Dump_Process();
 
-	Disassembler dis = Disassembler(&debug);
-	dis.Init(debug.scanner->Find_Pattern("48 8b 04 c1 48 8b 1c 03 48 8b cb 48 8b 03 ff 90 98 00 00 00"));
-	dis.Dump_ClientInfo();
-	system("pause");
+	while (true)
+	{
+		Disassembler dis = Disassembler(&debug);
+		dis.Init(debug.scanner->Find_Pattern("48 8b 04 c1 48 8b 1c 03 48 8b cb 48 8b 03 ff 90 98 00 00 00"));
+		dis.Dump_ClientInfo();
+		system("pause");
+	}
 }
