a4d4a843262f474bfa4e2aa2f0a8181a059e82ae
Mats Büscher <mats.buescher@gmx.de>
2023-02-11 22:47:11 +0100

    updated patterns

--- DecryptionDumper/DecryptionDumper.cpp
+++ DecryptionDumper/DecryptionDumper.cpp
@@ -50,8 +50,8 @@ int main()
 		case ModernWarfare:
 		case ModernWarfare2:
 			dis.Dump_Offsets_MW();
-			dis.Dump_ClientInfo_MW(debug.scanner->Find_Pattern("48 8B 4C 24 ? BA ? ? ? ? 0F B7 81 ? ? ? ? 66 83 E8 13 66 85 C2")); // OG , FF 90 ?? ?? ?? ?? 84 C0 0F 84 ?? ?? ?? ?? 48 8B 1D ?? ?? ?? ?? C6 44 24 ?? ?? 0F B6 44 24
-			dis.Dump_ClientBase(debug.scanner->Find_Pattern("48 8B 93 ? ? ? ? 90 C6 45"));
+			dis.Dump_ClientInfo_MW(debug.scanner->Find_Pattern("48 8B 4C 24 ? BA ? ? ? ? 0F B7 41 68 66 83 E8 13 66 85 C2"));
+			dis.Dump_ClientBase(debug.scanner->Find_Pattern("4C 8B 83 ? ? ? ? 90 C6 44 24 ? ? 0F B6 44 24 ? C0 C0 F8 0F B6 C0 65 4C 8B 18 4D 85 C0"));
 			break;
 		case Vanguard:
 			dis.Dump_ClientInfo_Vanguard(debug.scanner->Find_Pattern("48 8B 83 ?? ?? ?? ?? C6 44 24 ?? ?? 0F B6 4C 24 ?? C0"));
